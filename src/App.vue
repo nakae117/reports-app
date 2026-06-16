c26d23aac8e671207477a8514d36279903050bff
Edgar Carrizalez <edg.sistemas@gmail.com>
2026-06-16 19:05:20 -0400

    Deploy settings

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Reports App" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -16,8 +16,6 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
